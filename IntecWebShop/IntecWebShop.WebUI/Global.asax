35234ac783ef24cabc1f5f10b0c56b3dbbc7aeca
joost troost <joost_troost@hotmail.com>
2019-06-04 11:38:42 +0200

    1.00 Initial Commit Created Projects

---
+++ IntecWebShop/IntecWebShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntecWebShop.WebUI.MvcApplication" Language="C#" %>
---
+++ IntecWebShop/IntecWebShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IntecWebShop.WebUI.Startup))]
+namespace IntecWebShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
